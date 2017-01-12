6b09e9bb50f262ef272b04b7585f80c2d384e67c
David Muena H <dmuena@gmail.com>
2017-01-12 12:17:34 -0300

    add play route

--- saddam.py
+++ saddam.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/play", methods=['GET', 'POST'])
+def play():
+    return render_template("juego.html", word='_H_O_L_A')
+
 if __name__ == '__main__':
     app.run()
