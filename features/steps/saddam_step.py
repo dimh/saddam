a89637dc668475ae58ab35fc7437c261fca7c057
David Muena H <dmuena@gmail.com>
2017-01-12 14:39:49 -0300

    refactor load pages

--- features/steps/saddam_step.py
+++ features/steps/saddam_step.py
@@ -3,22 +3,10 @@ from behave import *
 from splinter import Browser
 browser = Browser('flask', app=app)
 
-@given(u'load initial page')
-def step_impl(context):
-    browser.visit('http://localhost:5000')
-
-@given(u'load game page')
-def step_impl(context):
-    browser.visit('http://localhost:5000/play')
-
 @then(u'show text "{text}"')
 def step_impl(context, text):
     assert text in browser.html, browser.html
 
-@given(u'load win page')
-def step_impl(context):
-    browser.visit('http://localhost:5000/win')
-
-@given(u'load lose page')
-def step_impl(context):
-    browser.visit('http://localhost:5000/lose')
+@given(u'load page: "{page}"')
+def step_impl(context, page):
+    browser.visit(page)    
