6e893b230a6f2dd89785cc6b0349a33b45505fff
David Muena H <dmuena@gmail.com>
2017-01-12 14:40:57 -0300

    add lose and win routes

--- saddam.py
+++ saddam.py
@@ -1,6 +1,6 @@
 import sys
 from flask import Flask, render_template, session
-app = Flask(__name__)
+app = Flask(__name__, static_url_path = "", static_folder = "templates/static")
 
 @app.route("/")
 def index():
@@ -9,6 +9,14 @@ def index():
 @app.route("/play", methods=['GET', 'POST'])
 def play():
     return render_template("juego.html", word='_H_O_L_A')
+
+@app.route("/lose", methods=['GET'])
+def lose():
+    return render_template("lose.html")
+
+@app.route("/win", methods=['GET'])
+def win():
+    return render_template("win.html")
 
 if __name__ == '__main__':
     app.run()
